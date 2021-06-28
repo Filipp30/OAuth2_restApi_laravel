87cdcbd833f29c13d40fcbe7fcd162b6926289b0
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-06-28 21:07:50 +0200

    handle Fb user data

--- OAuthServer/app/Http/Controllers/Auth/LoginWithFacebookController.php
+++ OAuthServer/app/Http/Controllers/Auth/LoginWithFacebookController.php
@@ -3,50 +3,51 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\UserFacebookCredentials;
+use Exception;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginWithFacebookController extends Controller{
 
+    private $callback_credentials;
+    private $fb_user_exist;
+    private $save_fb_credentials;
+
     public function get_facebook_redirect_url(){
-        return response(
-            [
-                'auth'=>Socialite::driver('facebook')->stateless()->redirect()->getTargetUrl()
-            ],
-            201
-        );
+        return response(['redirect_fb_url'=>Socialite::driver('facebook')
+        ->stateless()->redirect()->getTargetUrl()], 201);
     }
 
     public function auth_facebook_callback(){
-//        try {
-//            $user = Socialite::driver('facebook')->user();
-//            $isUser = User::query()->where('fb_id', $user->id)->first();
-
-//            if ($isUser){
-//                return response([
-//                    'message'=>'user facebook in DB exist',
-//                    'user_info_fb'=>$user,
-//                    'user_info_db'=>$isUser
-//                ],201);
-//            }else{
-//                return response([
-//                    'message'=>'user not exist in us DB',
-//                    'user_fb_info'=>$user,
-//                    'password'=> encrypt('user')
-//                ],201);
-//            }
-
-//            return redirect('/user');
-
-//        }catch (Exception $e){
-//            dd($e->getMessage());
-//        }
-        $user =  Socialite::driver('facebook')->stateless()->user();
-        dd($user);
-//        return view('user',['user'=>$user->email]);
+        try {
+            $this->callback_credentials = Socialite::driver('facebook')->stateless()->user();
+            $this->handle_fb_credentials();
+        }catch (Exception $e){
+            dd($e->getMessage());
+        }
     }
 
-    public function test(){
-        return 'Hello Wolrd!!!';
+    public function handle_fb_credentials(){
+
+        $this->fb_user_exist = UserFacebookCredentials::query()
+        ->where('fb_id','=',$this->callback_credentials->id)->first();
+
+        if (!$this->fb_user_exist){
+            $this->save_fb_credentials = UserFacebookCredentials::query()->create([
+                'fb_id'=>$this->callback_credentials->id,
+                'nickname'=>$this->callback_credentials->nickname,
+                'name'=>$this->callback_credentials->name,
+                'email'=>$this->callback_credentials->email,
+                'avatar'=>$this->callback_credentials->avatar_original,
+                'fb_profile_url'=>$this->callback_credentials->profileUrl
+            ])->save();
+        }
+
+        if ($this->fb_user_exist || $this->save_fb_credentials){
+            dd($this->fb_user_exist , $this->save_fb_credentials);
+            //response with Tokens
+        }
     }
+
 }

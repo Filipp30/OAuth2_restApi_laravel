21c8fbbbd03866678d8d4c3cf350d2025405639c
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-06-28 21:12:40 +0200

    set user_id field in FB-model if user_email equals fb_email

---
+++ OAuthServer/app/Providers/FacebookModelProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use App\Models\UserFacebookCredentials;
+use Illuminate\Support\ServiceProvider;
+
+class FacebookModelProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        //
+    }
+
+
+    public function boot(){
+
+        UserFacebookCredentials::creating(function($model){
+            $user = User::query()->where('email','=',$model->email)->first();
+            if ($user){
+                $model->user_id = $user->id;
+            }
+        });
+    }
+}
